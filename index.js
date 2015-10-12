0559a58163aaec3464529d889e9b317327b4207c
sterpe <srterpe@gmail.com>
2015-10-12 00:49:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = function LineSource(line) {
+	let position = 0;
+
+	if (line.split('\n').length > 1) {
+		throw new Error();
+	}
+
+	return Object.defineProperty(Object.defineProperty({
+		peekChar: function () {
+			return position + 1 < line.length ?
+				line.charAt(position + 1) :
+				null;
+		},
+		nextChar: function () {
+			return ++position, this.currentChar;
+		}
+	}, 'currentChar', {
+		get: function () {
+			return (position < line.length) ?
+				line.charAt(position) : null;
+		},
+		configurable: false
+	}), 'position', {
+		get: function () {
+			return (position < line.length) ?
+				position + 1 : null;
+		},
+		configurable: false
+	});
+}
