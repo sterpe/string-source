0afe29233fcba885f9d67ff2f0a10c1ec6cdd208
sterpe <srterpe@gmail.com>
2015-10-27 23:35:25 -0700

    Complete unit test coverage.

--- __tests__/line-source-test.js
+++ __tests__/line-source-test.js
@@ -30,7 +30,7 @@ describe([
 })
 
 describe([
-  'LineSource#currentChar'
+  'LineSource.currentChar'
 ].join(' '), function () {
   it([
     'should return the first',
@@ -78,10 +78,80 @@ describe([
 
     expect(l.currentChar).toBe(null)
 
-    let i
+    let i = 0
     for (; i < 100; i++) {
       l.nextChar()
       expect(l.currentChar).toBe(null)
     }
+  })
+})
+describe([
+  'LineSource#nextChar'
+].join(' '), function () {
+  it([
+    'should return each next',
+    'character and then null',
+    'afterwards'
+  ].join(' '), function () {
+    const LineSource = require(FILE)
+    const source = 'foo'
+
+    const l = new LineSource(source)
+
+    expect(l.nextChar()).toBe('o')
+    expect(l.nextChar()).toBe('o')
+    expect(l.nextChar()).toBe(null)
+
+    let i = 0
+    for (; i < 100; i++) {
+      expect(l.nextChar()).toBe(null)
+    }
+  })
+  describe([
+    'LineSource#peekChar'
+  ].join(' '), function () {
+    it([
+      'should return the character',
+      'that will be returned by the',
+      'next call to #nextChar'
+    ].join(' '), function () {
+      const LineSource = require(FILE)
+      const source = 'bar'
+
+      const l = new LineSource(source)
+
+      let i = 0
+
+      for (; i < 100; i++) {
+        expect(l.peekChar()).toBe(l.nextChar())
+      }
+    })
+  })
+  describe([
+    'LineSource.position'
+  ].join(' '), function () {
+    it([
+      'should return the current',
+      'position on the line and',
+      'null once past the end of',
+      'the line (position is 1-indexed)'
+    ].join(' '), function () {
+      const LineSource = require(FILE)
+      const source = 'quux'
+
+      const l = new LineSource(source)
+
+      let i = 0
+
+      for (; i < source.length; ++i) {
+        expect(l.currentChar).toBe(source.charAt(i))
+        expect(l.position).toBe(i + 1)
+        l.nextChar()
+      }
+      for (; i < 100; ++i) {
+        expect(l.position).toBe(null)
+        l.nextChar()
+      }
+    })
   })
 })
